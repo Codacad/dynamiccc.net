8dd5af3b4502480a217b3d60d1542eb3f5efe1a4
Mohd Rizwan <iamrizwan40@gmail.com>
2025-03-10 00:52:09 +0300

    added js interactivity in mobile-menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,16 @@
 window.addEventListener("load", function () {
   document.getElementById("loader").style.display = "none";
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const oponMobileMenu = document.querySelector(".open-mobile-menu");
+  const closeMobileMenu = document.querySelector(".close-mobile-menu");
+  oponMobileMenu.addEventListener("click", () => {
+    console.log("clicked");
+    document.querySelector(".mobile-menu").classList.toggle("show-mobile-menu");
+  });
+  closeMobileMenu.addEventListener("click", () => {
+    console.log("clicked");
+    document.querySelector(".mobile-menu").classList.toggle("show-mobile-menu");
+  });
+});
